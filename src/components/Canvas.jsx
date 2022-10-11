d15e2af4d62d5e647086e434d043d94746900e04
learningreac <learningreact2020@gmail.com>
2022-10-11 03:33:56 -0700

    full size grid

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -14,6 +14,9 @@ const Canvas = (props) => {
    // console.log('window', window.innerWidth, window.innerHeight)
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight * 0.9;
+    const grid_size = canvas.height /10;
+    const x = Math.floor(canvas.height / grid_size);
+    const y = Math.floor(canvas.width / grid_size);
 
    // console.log('board', board)
     const m = board.length;
@@ -22,22 +25,22 @@ const Canvas = (props) => {
 
     const draw = (ctx, board) => {
      
-      for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
+      for (let i = 0; i < x; i++) {
+        for (let j = 0; j < y; j++) {
 
-          if (board[i][j] === 1) {
+          if (i<m && j<n && board[i][j] === 1) {
             ctx.fillStyle = 'yellow'
           } else {
-            ctx.fillStyle = 'grey'
+            ctx.fillStyle = 'lightgrey'
           }
 
-          ctx.fillRect(j * 50, i * 50, 50, 50);
-          ctx.strokeRect(j * 50, i * 50, 50, 50);
+          ctx.fillRect(j * grid_size, i * grid_size, grid_size, grid_size);
+          ctx.strokeRect(j * grid_size, i * grid_size, grid_size, grid_size);
         }
       }
     }
 
-    if (board) {
+    if (board && canvas) {
       draw(ctx, board)
     }
 
